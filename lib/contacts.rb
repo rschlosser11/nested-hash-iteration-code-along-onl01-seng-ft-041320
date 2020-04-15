6a805d1ac196dbd4344f62e6733098c1f49bd460
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-15 21:28:04 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, info|
+    info.each do |key, value|
+      if key == :favorite_ice_cream_flavors
+        value.delete("strawberry")
+      end
+    end
+  end
 end
